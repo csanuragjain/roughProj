1514362849393aae0839444708f0aeb4f12a9773
Anurag Jain <cs.anurag.jain@gmail.com>
2018-05-23 13:04:25 +0530

    Update c.py

--- c.py
+++ c.py
@@ -26,6 +26,7 @@ t1 = datetime.now()
 try:
     for port in range(1,65535):  
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(5)
         result = sock.connect_ex((remoteServerIP, port))
         if result == 0:
             banner = sock.recv(1024)
